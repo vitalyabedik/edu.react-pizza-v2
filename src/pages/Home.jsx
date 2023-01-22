1d904d3d8c6d93ebaff9ad164265dcaa89582172
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-22 20:01:25 +0300

    feat: finish #7 video - add Skeleton, pages, React Router Dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Sort from '../components/Sort';
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://63cce64dfba6420d4d655b9d.mockapi.io/items')
+      .then(res => res.json())
+      .then(data => {
+        setItems(data);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className='container'>
+        <div className='content__top'>
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className='content__title'>Все пиццы</h2>
+        <div className='content__items'>
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+            : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
