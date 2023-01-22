944b8c6ce97d660cb3a651dd911fe757c70dc1df
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-22 11:38:30 +0300

    feat: finish #6 video - fetch data from backend, useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63cce64dfba6420d4d655b9d.mockapi.io/items')
+      .then(res => res.json())
+      .then(data => setItems(data));
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
@@ -21,7 +27,7 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {pizzas.map(obj => (
+            {items.map(obj => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
