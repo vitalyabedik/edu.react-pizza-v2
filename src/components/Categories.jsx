0e377a489f673431f767d0ce8e6340ba6d4bfafc
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-23 13:47:13 +0300

    feat: finish #9 video - add filter and sort logic for pizzas using React (state lifting)

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
-
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+const Categories = ({ value, onChangeCategory }) => {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
-
-  const onClickCategory = index => {
-    setActiveIndex(index);
-  };
 
   return (
     <div className='categories'>
       <ul>
-        {categories.map((category, index) => (
+        {categories.map((categoryName, index) => (
           <li
             key={index}
-            onClick={() => onClickCategory(index)}
-            className={activeIndex === index ? 'active' : ''}>
-            {category}
+            onClick={() => onChangeCategory(index)}
+            className={value === index ? 'active' : ''}>
+            {categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,35 @@ import Sort from '../components/Sort';
 const Home = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setSortType] = useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
   useEffect(() => {
-    fetch('https://63cce64dfba6420d4d655b9d.mockapi.io/items')
+    setIsLoading(true);
+
+    const getItems = 'https://63cce64dfba6420d4d655b9d.mockapi.io/items';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sortType.sortProperty.replace('-', '');
+
+    fetch(`${getItems}?${category}&sortBy=${sortBy}&order=${order}`)
       .then(res => res.json())
       .then(data => {
         setItems(data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
 
   return (
     <>
       <div className='container'>
         <div className='content__top'>
-          <Categories />
-          <Sort />
+          <Categories value={categoryId} onChangeCategory={id => setCategoryId(id)} />
+          <Sort value={sortType} onChangeSort={id => setSortType(id)} />
         </div>
         <h2 className='content__title'>Все пиццы</h2>
         <div className='content__items'>
