faef6887a6419b8ae7c539a0590b44c75185219c
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-23 21:28:58 +0300

    feat: finish #10 video - add searching and pagination pizzas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
@@ -7,12 +9,14 @@ import NotFound from './pages/NotFound';
 import './scss/app.scss';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <div className='wrapper'>
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className='content'>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home searchValue={searchValue} />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
+
 import Categories from '../components/Categories';
+import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
-import Sort from '../components/Sort';
+import Pagination from '../components/Pagination';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -20,15 +23,21 @@ const Home = () => {
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sortType.sortProperty.replace('-', '');
+    const search = searchValue ? `search=${searchValue}` : '';
 
-    fetch(`${getItems}?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(
+      `${getItems}?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`,
+    )
       .then(res => res.json())
       .then(data => {
         setItems(data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
   return (
     <>
@@ -38,11 +47,8 @@ const Home = () => {
           <Sort value={sortType} onChangeSort={id => setSortType(id)} />
         </div>
         <h2 className='content__title'>Все пиццы</h2>
-        <div className='content__items'>
-          {isLoading
-            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-            : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-        </div>
+        <div className='content__items'>{isLoading ? skeletons : pizzas}</div>
+        <Pagination onChangePage={number => setCurrentPage(number)} />
       </div>
     </>
   );
