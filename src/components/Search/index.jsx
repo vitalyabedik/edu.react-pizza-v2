6c62b64c63727d539c03d06fa9c7298d0bd32ece
Vitali Bedik <vitalya.bedik@gmail.com>
2023-01-24 09:42:44 +0300

    feat: finish #11 video - work with React Context

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { SearchContext } from '../../App';
+
 import styles from './Search.module.scss';
 
-const Search = ({ searchValue, setSearchValue }) => {
+const Search = () => {
+  const { searchValue, setSearchValue } = useContext(SearchContext);
+
   return (
     <div className={styles.root}>
       <svg className={styles.icon} viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'>
